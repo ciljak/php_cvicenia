40c5b61112a058c615dac09e31225dc67e6162fc
ciljak <ciljak@gmail.com>
2021-02-06 17:09:42 +0100

    Exercises for PHP & SQL - v. 0.1
    First introductory exercise focused on embedding php scripts and way how to use variables has been created.

---
+++ cv_2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ex. 2. PHP & SQL - expressions and string operation.</title>
+<link rel="stylesheet" href="style.css">
+
+
+</head>
+<body>
+<div class="container">
+
+
+  
+    <h1 id="Nadpis_headline">PHP language - 2. expression and string operation</h1> <!-- Príklad vloženia nadpisu prvej úrovne -->
+
+    <h2>How to embeed PHP code into a page?</h2>
+
+   
+
+
+      <div class="articles">
+        
+        <?php
+              echo "<p><em> Hello world </em> from our script :-)<p> "; // create example how to echoe output from embedded php script
+        ?>  
+
+      </div>  
+</div>
+
+</body>
+</html>
